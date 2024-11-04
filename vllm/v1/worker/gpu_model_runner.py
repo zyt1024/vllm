fc13e3df8cd2d579bd1a3f85b9f22292391775e5
Woosuk Kwon <woosuk.kwon@berkeley.edu>
2024-11-03 21:58:59 -0800

    Remove comment

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -35,7 +35,6 @@ class GPUModelRunner:
         vllm_config: VllmConfig,
         input_registry: InputRegistry = INPUT_REGISTRY,
     ):
-        # TODO: use ModelRunnerBase.__init__(self, vllm_config=vllm_config)
         self.vllm_config = vllm_config
         self.model_config = vllm_config.model_config
         self.cache_config = vllm_config.cache_config
